3bfb2b67ba1aae677ce16433634fe81fddd43a48
Newton Duarte Cavalcanti <newton_@hotmail.com>
2026-05-20 17:04:03 -0300

    feat: add pause all function on game over and reload current scene on escape press

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 const DICE = preload("res://scenes/dice.tscn")
 
+const STOPPABLE_GROUP: String = "stoppable"
 const MARGIN: float = 80.0
 
 @onready var spawn_timer: Timer = $SpawnTimer
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
 
 func _ready() -> void:
 	spawn_dice()
@@ -20,5 +25,11 @@ func spawn_dice() -> void:
 	new_dice.game_over.connect(_on_dice_game_over)
 	add_child(new_dice)
 
+func pause_all() -> void:
+	spawn_timer.stop()
+	var to_stop: Array[Node] = get_tree().get_nodes_in_group(STOPPABLE_GROUP)
+	for item in to_stop:
+		item.set_physics_process(false)
+
 func _on_dice_game_over() -> void:
-	print("GAME OVER")
+	pause_all()
