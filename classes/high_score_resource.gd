c0b5ee41457734a867525d051abfe324bf161159
Newton Duarte Cavalcanti <newton_@hotmail.com>
2026-05-22 19:50:51 -0300

    feat: persist high score to user folder

---
+++ classes/high_score_resource.gd
@@ -0,0 +1,5 @@
+class_name HighScoreResource
+
+extends Resource
+
+@export var high_score: int = 0
--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -1,8 +1,25 @@
 extends Node
 
+const SCORES_PATH: String = "user://dice_catcher_score.res"
+
 var high_score: int = 0:
 	get:
 		return high_score
 	set(value):
 		if value > high_score:
 			high_score = value
+			save_high_core()
+
+func _ready() -> void:
+	load_high_score()
+
+func load_high_score() -> void:
+	if ResourceLoader.exists(SCORES_PATH):
+		var saved_high_score: HighScoreResource = load(SCORES_PATH)
+		if saved_high_score:
+			high_score = saved_high_score.high_score
+
+func save_high_core() -> void:
+	var high_score_to_save: HighScoreResource = HighScoreResource.new()
+	high_score_to_save.high_score = high_score
+	ResourceSaver.save(high_score_to_save, SCORES_PATH)
