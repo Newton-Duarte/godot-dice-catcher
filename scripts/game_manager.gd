c9413d5862f91d94be97a1208efe2ba5fdc32af3
Newton Duarte Cavalcanti <newton_@hotmail.com>
2026-05-23 20:16:53 -0300

    feat: add toggle fullscreen

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,6 +6,15 @@ const LOADING = preload("uid://bhxltqtcpw5uh")
 
 var next_scene: PackedScene
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		var current_mode = DisplayServer.window_get_mode()
+		if current_mode == DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		elif current_mode == DisplayServer.WindowMode.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
+
 func load_next_scene() -> void:
 	get_tree().change_scene_to_packed(next_scene)
 
