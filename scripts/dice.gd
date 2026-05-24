42d6d225943a450b5afbd887bd302efb66c05cd8
Newton Duarte Cavalcanti <newton_@hotmail.com>
2026-05-23 21:25:37 -0300

    feat: add particles to dice

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -5,6 +5,7 @@ extends Area2D
 signal off_screen(dice: Dice)
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 
 const SPEED: float = 150.0
 const ROTATION_SPEED: float = 5.0
@@ -13,13 +14,17 @@ var rotate_directions: Array[float] = [-1.0, 1.0]
 var rotate_direction: float = 1.0
 var speed_multiplier: float = 1.0
 var points: int = 1
-var dice_rare_color: Color = Color("#FFD700")
-var dice_bad_color: Color = Color("#D63031")
 var is_rare: bool = false
 var is_bad: bool = false
 
 func _ready() -> void:
 	rotate_direction = rotate_directions.pick_random()
+	if is_rare:
+		modulate = ColorManager.RARE_COLOR
+		cpu_particles_2d.color = ColorManager.RARE_COLOR
+	elif is_bad:
+		modulate = ColorManager.BAD_COLOR
+		cpu_particles_2d.color = ColorManager.BAD_COLOR
 
 func _physics_process(delta: float) -> void:
 	sprite_2d.rotate((ROTATION_SPEED * rotate_direction) * delta)
@@ -28,11 +33,9 @@ func _physics_process(delta: float) -> void:
 func setup_rare_dice() -> void:
 	is_rare = true
 	points = 5
-	modulate = dice_rare_color
 
 func setup_bad_dice() -> void:
 	is_bad = true
-	modulate = dice_bad_color
 
 func _on_screen_exited() -> void:
 	off_screen.emit(self)
